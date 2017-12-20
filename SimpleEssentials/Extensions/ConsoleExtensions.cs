666e95fa80f6799e15cd3a6cc53dad9d7276a2ce
Kevin Suarez <kevin.suarez@crackerbarrel.com>
2017-12-20 08:36:36 -0600

    Add console extensions

---
+++ SimpleEssentials/Extensions/ConsoleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEssentials.Extensions
+{
+    public static class ConsoleEx
+    {
+        public static void WriteLine(string text, ConsoleColor color = ConsoleColor.Yellow)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
