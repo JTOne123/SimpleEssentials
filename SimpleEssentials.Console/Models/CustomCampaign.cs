4694c73bfff1cb1c5de26aa7299b773d181922d6
Kevin Suarez <kevin.suarez@crackerbarrel.com>
2018-01-26 16:45:31 -0600

    Add linqToSql to Get method. Clean a few files

--- SimpleEssentials.Console/Models/CustomCampaign.cs
+++ SimpleEssentials.Console/Models/CustomCampaign.cs
@@ -9,6 +9,7 @@ namespace SimpleEssentials.Console.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public bool Test { get; set; }
         public DateTime CreateDate { get; set; }
     }
 }
